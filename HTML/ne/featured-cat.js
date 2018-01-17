44f57e4eb60eaeb92813d4ab136fcce88cd690c9
nesparza <nesparza@nesparzaMacBookPro.local>
2018-01-17 10:19:26 -0500

    Featured Categories

--- HTML/ne/featured-cat.js
+++ HTML/ne/featured-cat.js
@@ -1,3 +1,29 @@
-$(document).ready(function(){
-    $(".row.featured-link-container").addClass("fat-cat");
+$(document).ready(function () {
+    //my code
+
+    $slick_slider = $('.slick');
+    settings = {
+        // some settings
+        dots: true,
+        arrows:false,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        adaptiveHeight:true
+    }
+    $slick_slider.slick(settings);
+
+    // reslick only if it's not slick()
+    $(window).on('resize', function () {
+        if ($(window).width() > 768) {
+            if ($slick_slider.hasClass('slick-initialized')) {
+                $slick_slider.slick('unslick');
+            }
+            return
+        }
+
+        if (!$slick_slider.hasClass('slick-initialized')) {
+            return $slick_slider.slick(settings);
+        }
+    });
 });
