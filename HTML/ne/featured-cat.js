4b18a5371e9a79343aaf429f88812b881a37e5f9
nesparza <nesparza@nesparzaMacBookPro.local>
2018-01-23 11:58:54 -0500

    featured categories

--- HTML/ne/featured-cat.js
+++ HTML/ne/featured-cat.js
@@ -1,29 +1,54 @@
 $(document).ready(function () {
-    //my code
-
-    $slick_slider = $('.slick');
-    settings = {
-        // some settings
-        dots: true,
-        arrows:false,
-        speed: 500,
-        fade: true,
-        cssEase: 'linear',
-        adaptiveHeight:true
+    var isMobile = false;
+    var URLpathArray = window.location.pathname.split('/');
+    if (URLpathArray[1].toLowerCase() === "mobile" || URLpathArray[2].toLowerCase() === "gs" || window.location.hostname === "m.barnesandnoble.com" || window.location.hostname === "mbarnesandnoble.skavaone.com" || window.location.hostname === "mpreprod.barnesandnoble.com" || window.location.hostname === "mpreview.barnesandnoble.com") {
+        isMobile = true;
     }
-    $slick_slider.slick(settings);
 
-    // reslick only if it's not slick()
-    $(window).on('resize', function () {
-        if ($(window).width() > 768) {
-            if ($slick_slider.hasClass('slick-initialized')) {
-                $slick_slider.slick('unslick');
+    if (isMobile) {
+        $('.row.featured-link-container').remove('.clearfix');
+        $('.row.featured-link-container').addClass('slickFeatCat');
+
+        var slickFeatCat = function () {
+            $slick_slider = $('.slickFeatCat');
+
+            settings = {
+                // slick settings
+                dots: true,
+                arrows: false,
+                speed: 500,
+                fade: true,
+                cssEase: 'linear',
+                adaptiveHeight: true,
+                respondTo: 'window'
+
             }
-            return
-        }
 
-        if (!$slick_slider.hasClass('slick-initialized')) {
-            return $slick_slider.slick(settings);
+            $slick_slider.slick(settings);
         }
-    });
+        // reslick only if it's not slick()
+        $(window).on('resize', function () {
+
+            if ($(window).width() > 768) {
+
+                $(".slick").addClass('fatcat');
+                if ($slick_slider.hasClass('slick-initialized')) {
+                    $slick_slider.slick('unslick');
+                }
+                return
+            } else {
+                slickFeatCat();
+            }
+
+            if (!$slick_slider.hasClass('slick-initialized')) {
+                slickFeatCat();
+
+                return $slick_slider.slick(settings);
+            }
+
+        });
+        if ($(window).width() < 768) {
+            slickFeatCat();
+        }
+    }
 });
